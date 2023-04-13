63f6f72b4b576f4709bc3613225ebeff59d595ba
bataino <bataino.ronaldo@gmail.com>
2023-04-13 22:34:26 +0100

    first comiit

---
+++ index.py
@@ -0,0 +1,23 @@
+import urllib.request
+from flask import Flask, jsonify, request
+import json
+from pyresparser import ResumeParser
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    resume = request.args.get('resume')
+    resume = resume or "https://res.cloudinary.com/dhtgqzkse/image/upload/v1681132590/personal/Resume-AbdulBateen-Jolaoshoo_djmeh0.pdf"
+    res = resume.split(".")
+    ext = res[len(res) - 1]
+    
+    print([res, resume, ext])
+    
+    urllib.request.urlretrieve(resume, "resume." + ext )
+    data = ResumeParser("resume."+ext).get_extracted_data()
+    
+    print(data)
+    return jsonify(data)
+app.run()
+# 66586986647
